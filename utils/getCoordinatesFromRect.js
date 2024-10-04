a960f3810d8f42fb36ff366f811782cae76f5dc4
Chenzorama <chenzorama@hotmail.com>
2024-10-04 14:05:06 -0400

    got REKT

--- utils/getCoordinatesFromRect.js
+++ utils/getCoordinatesFromRect.js
@@ -3,38 +3,48 @@ function getCoordinatesFromRect(rect, showDots = false) {
   const y = parseFloat(rect.getAttribute('y'));
   const width = parseFloat(rect.getAttribute('width'));
   const height = parseFloat(rect.getAttribute('height'));
-  const transform =
-    rect.transform.baseVal.numberOfItems > 0
-      ? rect.transform.baseVal.consolidate().matrix
-      : { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0 };
 
-  const vertices = [
-    { x: x, y: y },
-    { x: x + width, y: y },
-    { x: x + width, y: y + height },
-    { x: x, y: y + height },
-  ];
-
-  const dotsGroup = document.getElementById('DOTS');
   const coordinates = [];
 
-  vertices.forEach((vertex) => {
-    const transformedVertex = {
-      x: vertex.x * transform.a + vertex.y * transform.c + transform.e,
-      y: vertex.x * transform.b + vertex.y * transform.d + transform.f,
-    };
+  if (rect.transform.baseVal.length > 0) {
+    const transform =
+      rect.transform.baseVal.numberOfItems > 0
+        ? rect.transform.baseVal.consolidate().matrix
+        : { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0 };
 
-    if (showDots) {
-      const greenDot = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-      greenDot.setAttribute('cx', transformedVertex.x);
-      greenDot.setAttribute('cy', transformedVertex.y);
-      greenDot.setAttribute('r', '3');
-      greenDot.setAttribute('fill', 'green');
-      dotsGroup.appendChild(greenDot);
-    }
+    const vertices = [
+      { x: x, y: y },
+      { x: x + width, y: y },
+      { x: x + width, y: y + height },
+      { x: x, y: y + height },
+    ];
 
-    coordinates.push({ x: transformedVertex.x, y: transformedVertex.y });
-  });
+    const dotsGroup = document.getElementById('DOTS');
+
+    vertices.forEach((vertex) => {
+      const transformedVertex = {
+        x: vertex.x * transform.a + vertex.y * transform.c + transform.e,
+        y: vertex.x * transform.b + vertex.y * transform.d + transform.f,
+      };
+
+      if (showDots) {
+        const greenDot = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        greenDot.setAttribute('cx', transformedVertex.x);
+        greenDot.setAttribute('cy', transformedVertex.y);
+        greenDot.setAttribute('r', '3');
+        greenDot.setAttribute('fill', 'green');
+        dotsGroup.appendChild(greenDot);
+      }
+
+      coordinates.push({ x: transformedVertex.x, y: transformedVertex.y });
+    });
+
+  } else {
+    coordinates.push({ x: x, y: y });
+    coordinates.push({ x: x + width, y: y });
+    coordinates.push({ x: x + width, y: y + height });
+    coordinates.push({ x: x, y: y + height });
+  }
 
   return coordinates;
 }
